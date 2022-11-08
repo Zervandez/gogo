43a8420f7adf624cba219b3409966a6726626f3f
Ariel-ThekenLaptop <pddq@proton.me>
2022-11-08 15:21:29 +0100

    structs

--- main.go
+++ main.go
@@ -7,4 +7,29 @@ import (
 func main() {
 	fmt.Println("Hello, world.")
 
+	Ariel := Person{true, "Ariel", 21, "Student", "Albaner", "Jewish"}
+
+	var ptr *bool = &Ariel.Gender
+
+	fmt.Println(ptr)
+	fmt.Println(Ariel.Name)
+
+}
+
+type Person struct {
+	Gender      bool
+	Name        string
+	age         int
+	occupation  string
+	nationality string
+	race        string
+}
+
+type Vehicle struct {
+	Make     string
+	Model    string
+	Year     int
+	FuelType string
+	price    float32
+	maxSpeed int
 }
